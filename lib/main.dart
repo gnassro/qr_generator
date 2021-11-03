cd7a354ce0997d94bde934f11d56a59539f6d3e8
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-03 11:52:33 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'blab'),
+      home: const MyHomePage(title: 'blabdd'),
     );
   }
 }
