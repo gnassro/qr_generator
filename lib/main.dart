9d6069037b4f8fade0418f0a890e6f6e70f873c1
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-03 11:12:54 +0100

    init master

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page master1'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
