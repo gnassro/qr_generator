f327bca811dcb939c353c2897d432fcbdf3601cb
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-05 08:39:54 +0100

    let start

--- lib/main.dart
+++ lib/main.dart
@@ -52,25 +52,22 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
           ),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text the that user has entered by using the
+                      // TextEditingController.
+                      content: Text(myController.text),
+                    );
+                  },
+                );
+              },
+              child: const Text("Generate")
+          )
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(myController.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.text_fields),
       ),
     );
   }
