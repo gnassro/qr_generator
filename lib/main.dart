f930da412d87a24d7b22854f6ed8af2cc872718d
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-10 20:18:59 +0100

    End Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,131 @@
-import 'dart:async';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
 import 'dart:io';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
-
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'capture_widget.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.pinkAccent,
-      ),
-      home: ExampleScreen(),
-    ),
-  );
-}
+void main() => runApp(const MyApp());
 
-class ExampleScreen extends StatefulWidget {
-  @override
-  _ExampleScreenState createState() => new _ExampleScreenState();
-}
-
-class _ExampleScreenState extends State<ExampleScreen> {
-  final _captureKey = GlobalKey<CaptureWidgetState>();
-  Future<CaptureResult>? _image;
-
-  void _onCapturePressed() {
-    setState(() {
-      _image = _captureKey.currentState!.captureImage();
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CaptureWidget(
-      key: _captureKey,
-      capture: QrImage(
-          constrainErrorBounds: false,
-          backgroundColor: Colors.white,
-          size: 500,
-          data: "hjus",
-          version: QrVersions.auto,
-          gapless: false,
-        ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget To Image Demo'),
-        ),
-        body: FutureBuilder<CaptureResult>(
-          future: _image,
-          builder: (BuildContext context, AsyncSnapshot<CaptureResult> snapshot) {
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Center(
-                    child: RaisedButton(
-                      child: Text('Capture Image'),
-                      onPressed: _onCapturePressed,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
+    return const MaterialApp(
+      title: 'QR Generator',
+      home: QrGenerateApp(),
     );
   }
 }
 
+
+class QrGenerateApp extends StatefulWidget {
+  const QrGenerateApp({Key? key}) : super(key: key);
+
+  @override
+  _QrGenerateAppState createState() => _QrGenerateAppState();
+}
+
+class _QrGenerateAppState extends State<QrGenerateApp> {
+  final myController = TextEditingController();
+  String? inputTextToGenerate;
+  Uint8List? pngBytes;
+
+  @override
+  void initState() {
+    super.initState();
+    inputTextToGenerate = "";
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Generate',
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  controller: myController,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    inputTextToGenerate = myController.text;
+                  });
+                },
+                child: const Text("Generate")),
+            _generateQRImage(inputTextToGenerate),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _capturePng(inputTextToGenerate);
+                  });
+                },
+                child: const Text("Download")),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _capturePng(String? textToGenerate, {double imageSize = 100}) async {
+    try {
+      var permStatus = await Permission.storage.status;
+      if (!permStatus.isGranted) {
+        await Permission.storage.request();
+      }
+
+      if(!permStatus.isDenied) {
+        final image = await QrPainter(
+            data: textToGenerate!,
+            version: QrVersions.auto,
+            gapless: false
+        ).toImageData(imageSize);
+
+        final directory = await getApplicationDocumentsDirectory();
+        final imagePath = await File('${directory.path}/${DateTime.now().millisecondsSinceEpoch.toString()}.png').create();
+        await imagePath.writeAsBytes(image!.buffer.asUint8List());
+
+        await ImageGallerySaver.saveFile(imagePath.path);
+      }
+
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Widget _generateQRImage(String? textToGenerate) {
+    if (textToGenerate != "") {
+      return QrImage(
+        constrainErrorBounds: false,
+        backgroundColor: Colors.white,
+        size: 500,
+        data: textToGenerate!,
+        version: QrVersions.auto,
+        gapless: false,
+      );
+    }
+    return const Text("");
+  }
+}
