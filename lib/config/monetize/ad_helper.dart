4a8dc927a2334dbc1212dd5775e2ec26d6d3071a
gnassro <38044719+gnassro@users.noreply.github.com>
2021-12-29 20:44:25 +0100

    update README

--- lib/config/monetize/ad_helper.dart
+++ lib/config/monetize/ad_helper.dart
@@ -4,8 +4,8 @@ import 'package:flutter_config/flutter_config.dart';
 class AdHelper {
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return FlutterConfig.get('ANDROID_INTERSTITIAL_AD_UNIT_ID');
-      //return "ca-app-pub-3940256099942544/5224354917";
+      return FlutterConfig.get('ANDROID_INTERSTITIAL_AD_UNIT_ID'); // you must define .dev file
+      //return "ca-app-pub-3940256099942544/5224354917"; // use this for the test
     } else {
       throw UnsupportedError("Unsupported platform");
     }
